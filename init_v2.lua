b0d0e3e2350593215be4fbc1446815ead8894773
John Dennis <jdennis925@gmail.com>
2025-07-06 14:28:55 -0500

    Create init_v2.lua

---
+++ init_v2.lua
@@ -0,0 +1,8 @@
+
+-- Enable relative line numbers
+vim.opt.number = true         -- Show absolute number on the current line
+vim.opt.relativenumber = true -- Show relative numbers on all other lines
+
+
+--themeing/color scheme
+vim.cmd.colorscheme("evening")
